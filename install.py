82d3a4c4bb7039a5102d218c3380629e7637cc73
Rui Vieira <ruidevieira@googlemail.com>
2016-02-20 16:02:56 +0000

    Better do it in Python....

---
+++ install.py
@@ -0,0 +1,27 @@
+#!env python
+import os
+from colors import red, green, blue
+import glob
+import shutil
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def is_sudo():
+    return os.getuid() == 0
+
+def copy(pattern, destination, message):
+    print green(message)
+    for file in glob.glob(pattern):
+        print file
+        shutil.copy(file, destination)
+
+def copy_scripts():
+    copy(pattern = CURRENT_DIR + r'\*',
+        destination = '/usr/local/bin',
+        message = 'Copying scripts to /usr/local/bin')
+
+if __name__ == "__main__":
+    if not is_sudo():
+        print red('This script needs sudo.')
+    else:
+        copy_scripts()
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!env sh
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run this as root."
-  exit
-fi
-
-# copy the files in bin
-cp bin/* /usr/local/bin
