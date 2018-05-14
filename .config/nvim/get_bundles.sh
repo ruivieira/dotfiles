8d0f13349d94b482cbc17ed804d7027e2cd49e55
Rui  Vieira <ruidevieira@googlemail.com>
2018-05-14 13:02:00 +0100

    Add script to fetch (Neo)vim bundles

---
+++ .config/nvim/get_bundles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BUNDLES=~/.config/nvim/bundle
+rm -Rf $BUNDLES/nerdtree
+git clone git://github.com/scrooloose/nerdtree.git $BUNDLES/nerdtree
+rm -Rf $BUNDLES/nim.vim
+git clone git://github.com/zah/nim.vim.git $BUNDLES/nim.vim
+rm -Rf $BUNDLES/syntastic
+git clone git://github.com/scrooloose/syntastic.git $BUNDLES/syntastic
+rm -Rf $BUNDLES/vim-airline
+git clone git://github.com/vim-airline/vim-airline.git $BUNDLES/vim-airline
+rm -Rf $BUNDLES/vim-fugitive
+git clone git://github.com/tpope/vim-fugitive.git $BUNDLES/vim-fugitive
+rm -Rf $BUNDLES/vim-gitgutter
+git clone git://github.com/airblade/vim-gitgutter.git $BUNDLES/vim-gitgutter
