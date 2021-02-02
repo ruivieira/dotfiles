630fb3417fc11bc83dc5408a049662c4f11cf83d
Rui Vieira <ruidevieira@googlemail.com>
2021-02-02 16:39:28 +0000

    Add wiki to backup scripts

--- .functions.zsh
+++ .functions.zsh
@@ -1,3 +1,6 @@
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
 function pyenvcreate () {
 	local PROJ_DIR=$(basename $(pwd))
 	echo creating pyenv: $fg[green]$PROJ_DIR $reset_color
@@ -25,9 +28,12 @@ function jvm_exists() {
 	fi
 }
 
-function rsync_code_dropbox() {
+function rsync_backup() {
 	cd ~/Sync/
+	echo -e "${GREEN}Sync code folders${NOCOLOR}"
 	rsync -avh --exclude-from='.stignore' --delete  ~/Sync/code ~/Dropbox/code/backup/
+	echo -e "${GREEN}Sync wiki folder${NOCOLOR}"
+	rsync -avh --exclude-from='.stignore' --delete  ~/Library/Containers/co.noteplan.NotePlan3/Data/Library/Application\ Support/co.noteplan.NotePlan3/ ~/Dropbox/wiki/
 }
 
 function docker_nuke() {
@@ -47,7 +53,12 @@ function b2_upload_backup() {
 }
 
 function restic_backup() {
+	echo -e "${GREEN}Backing up code${NOCOLOR}"
 	 restic -r b2:${B2_RESTIC_BUCKET}:/ --verbose backup ~/Dropbox/code/backup/code --exclude-file="${HOME}/Sync/.stignore"
+	 echo -e "${GREEN}Backing up documents${NOCOLOR}"
 	 restic -r b2:${B2_RESTIC_BUCKET}:/ --verbose backup ~/Sync/documents --exclude-file="${HOME}/Sync/.stignore"
+	 echo -e "${GREEN}Backing up sites${NOCOLOR}"
 	 restic -r b2:${B2_RESTIC_BUCKET}:/ --verbose backup ~/Sync/sites --exclude-file="${HOME}/Sync/.stignore"
+	 echo -e "${GREEN}Backing up wiki${NOCOLOR}"
+	 restic -r b2:${B2_RESTIC_BUCKET}:/ --verbose backup ~/Dropbox/wiki --exclude-file="${HOME}/Sync/.stignore"
 }
