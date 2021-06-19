33bf020984ecefca7bccb491f41d5d2a5aa4fe66
Rui Vieira <ruidevieira@googlemail.com>
2021-06-19 18:17:29 +0100

    Remove profile files
    Profile files are now installed as part of `dot` and `chezmoi`

--- .builders.zsh
+++
@@ -1,8 +0,0 @@
-QUARKUS_VERSION=1.2.1
-
-function quarkus-new() {
-	mvn io.quarkus:quarkus-maven-plugin:$QUARKUS_VERSION.Final:create \
-    -DprojectGroupId=$1 \
-    -DprojectArtifactId=$2 \
-    -Dextensions="kafka-streams,resteasy-jsonb"
-}
