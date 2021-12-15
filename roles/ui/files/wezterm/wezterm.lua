2163fc87c68b2901a23bb07c8065fea2f0a0c164
Rui Vieira <ruidevieira@googlemail.com>
2021-12-15 22:21:32 +0000

    Add Linux installation of wezterm

--- roles/ui/files/wezterm/wezterm.lua
+++ roles/ui/files/wezterm/wezterm.lua
@@ -3,15 +3,13 @@ local wezterm = require 'wezterm';
 return {
     color_scheme = "Arthur",
     font = wezterm.font("JetBrains Mono"),
-    font_size = 14,
+    font_size = 10,
     warn_about_missing_glyphs = false,
     use_dead_keys = false,
     default_cursor_style = "BlinkingBlock",
     cursor_blink_rate = 500,
     window_close_confirmation = "NeverPrompt",
-    keys = {{
-        key = "q",
-        mods = "CTRL",
-        action = "QuitApplication"
-    }}
+    keys = {
+        {key="q", mods="CTRL", action="QuitApplication"},
+      }
 }
