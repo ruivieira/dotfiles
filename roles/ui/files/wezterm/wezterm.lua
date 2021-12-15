95ecf5aca87fb2670a6ad6f74961fa4904356059
Rui Vieira <ruidevieira@googlemail.com>
2021-12-15 21:35:36 +0000

    Remove missing glyphs warning from wezterm

--- roles/ui/files/wezterm/wezterm.lua
+++ roles/ui/files/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ return {
     color_scheme = "Arthur",
     font = wezterm.font("JetBrains Mono"),
     font_size = 14,
+    warn_about_missing_glyphs = false,
     use_dead_keys = false,
     default_cursor_style = "BlinkingBlock",
     cursor_blink_rate = 500,
