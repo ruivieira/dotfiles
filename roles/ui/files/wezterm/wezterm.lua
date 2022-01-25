926c572dffa3dc1029fe1462264199b16971babb
Rui Vieira <ruidevieira@googlemail.com>
2022-01-25 17:57:06 +0000

    Update Wezterm configuration

--- roles/ui/files/wezterm/wezterm.lua
+++ roles/ui/files/wezterm/wezterm.lua
@@ -1,15 +1,17 @@
 local wezterm = require 'wezterm';
 
 return {
-    color_scheme = "Arthur",
-    font = wezterm.font("JetBrains Mono"),
-    font_size = 10,
+    color_scheme = "Spring",
+    font = wezterm.font("Comic Mono"),
+    font_size = 14,
     warn_about_missing_glyphs = false,
     use_dead_keys = false,
     default_cursor_style = "BlinkingBlock",
     cursor_blink_rate = 500,
     window_close_confirmation = "NeverPrompt",
     keys = {
-        {key="q", mods="CTRL", action="QuitApplication"},
-      }
+      {key="q", mods="CTRL", action="QuitApplication"},
+      -- This will create a new split and run the `top` program inside it
+      {key="d", mods="CTRL", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}}
+    }
 }
